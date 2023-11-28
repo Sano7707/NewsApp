cfb1682c91ab3c1aa18704b9e7ea865229e87d3e
Sano <sanasar.hambardzumyan@gmail.com>
2023-11-28 17:10:16 +0400

    Moved Strings to a Constants, adjusted gradle dependencies

---
+++ app/src/main/java/com/loc/newsapp/util/Constants.kt
@@ -0,0 +1,14 @@
+package com.loc.newsapp.util
+
+object Constants {
+
+    const val USER_SETTINGS = "user_settings"
+    const val APP_ENTRY = "app_entry"
+    const val FIRST_TITLE = "Israel-Hamas war live: Israeli army continues ‘pounding’ southern Gaza"
+    const val SECOND_TITLE = "US and China to resume high-level military communication"
+    const val THIRD_TITLE = "Armenia and Azerbaijan have agreed on basic peace treaty principles"
+    const val description1 = "About 31 Palestinians have been killed in separate strikes that took place at the Nuseirat ... "
+    const val description2 = "US President Joe Biden and Chinese leader Xi Jinping have agreed to resume high-level ..."
+    const val description3 = "The two countries have been at odds for decades, most notably over the breakaway region of ... "
+
+}
