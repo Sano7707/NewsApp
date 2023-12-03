0b5a76c97f9fade982931ac415b51203a5f5b900
Sano <sanasar.hambardzumyan@gmail.com>
2023-12-04 00:05:17 +0400

    Changed NavGraph according to route NewsNavigatorScreen

--- app/src/main/java/com/loc/newsapp/di/AppModule.kt
+++ app/src/main/java/com/loc/newsapp/di/AppModule.kt
@@ -2,16 +2,24 @@ package com.loc.newsapp.di
 
 import android.app.Application
 import com.loc.newsapp.data.manager.LocalUserMangerImpl
+import com.loc.newsapp.data.manager.remote.NewsApi
+import com.loc.newsapp.data.manager.repository.NewsRepositoryImpl
+import com.loc.newsapp.domain.manager.repository.NewsRepository
+import com.loc.newsapp.domain.manager.usecases.app_entry.AppEntryUseCases
+import com.loc.newsapp.domain.manager.usecases.app_entry.ReadAppEntry
+import com.loc.newsapp.domain.manager.usecases.app_entry.SaveAppEntry
+import com.loc.newsapp.domain.manager.usecases.news.GetNews
+import com.loc.newsapp.domain.manager.usecases.news.NewsUseCases
+import com.loc.newsapp.domain.manager.usecases.news.SearchNews
 import com.loc.newsapp.domain.manager.manager.LocalUserManager
-import com.loc.newsapp.domain.manager.usecases.AppEntryUseCases
-import com.loc.newsapp.domain.manager.usecases.ReadAppEntry
-import com.loc.newsapp.domain.manager.usecases.SaveAppEntry
+import com.loc.newsapp.util.Constants.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
-
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -31,5 +39,35 @@ object AppModule {
         readAppEntry = ReadAppEntry(localUserManger),
         saveAppEntry = SaveAppEntry(localUserManger)
     )
+
+    @Provides
+    @Singleton
+    fun provideApiInstance(): NewsApi {
+        return Retrofit
+            .Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(NewsApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNewsRepository(
+        newsApi: NewsApi
+    ): NewsRepository {
+        return NewsRepositoryImpl(newsApi)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNewsUseCases(
+        newsRepository: NewsRepository
+    ): NewsUseCases {
+        return NewsUseCases(
+            getNews = GetNews(newsRepository),
+            searchNews = SearchNews(newsRepository)
+        )
+    }
 
 }
--- app/src/main/java/com/loc/newsapp/presentation/navgraph/NavGraph.kt
+++ app/src/main/java/com/loc/newsapp/presentation/navgraph/NavGraph.kt
@@ -1,14 +1,21 @@
 package com.loc.newsapp.presentation.navgraph
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.navigation
 import androidx.navigation.compose.rememberNavController
-import com.loc.newsapp.presentation.onboarding.OnBoardingViewModel
+import androidx.navigation.navigation
+import com.loc.newsapp.presentation.home.HomeScreen
+import com.loc.newsapp.presentation.news_navigator.NewsNavigator
 import com.loc.newsapp.presentation.onboarding.OnBoardingScreen
-import androidx.hilt.navigation.compose.hiltViewModel
-
+import com.loc.newsapp.presentation.onboarding.OnBoardingViewModel
 
 @Composable
 fun NavGraph(
@@ -29,19 +36,10 @@ fun NavGraph(
 
         navigation(
             route = Route.NewsNavigation.route,
-            startDestination = Route.HomeScreen.route
+            startDestination = Route.NewsNavigatorScreen.route
         ) {
-            composable(route = Route.HomeScreen.route) {
-
-            }
-            composable(route = Route.SearchScreen.route) {
-
-            }
-            composable(route = Route.BookmarkScreen.route) {
-
-            }
-            composable(route = Route.DetailsScreen.route) {
-
+            composable(route = Route.NewsNavigatorScreen.route){
+                NewsNavigator()
             }
         }
     }
